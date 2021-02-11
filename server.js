340dce11e17b747a9373ab157521522c9ea30806
Carolina R <carolinarizk@gmail.com>
2021-02-11 08:16:09 -0500

    created server.js (reqs/exp, started express setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/apiroutes.js');
+const htmlRoutes = require('./routes/htmlroutes.js');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //send back the index.html
+  res.sendFile(path.join(`${__dirname}/public/index.html`));
+})
+
+//ROUTES
+app.use(`/api/notes`, apiRoutes);
+app.use(`/notes`, htmlRoutes);
+
+//
+
+// Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
