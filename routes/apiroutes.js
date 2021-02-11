8e9118147fe979ca3010a5e7b007c7184fd7c972
Carolina R <carolinarizk@gmail.com>
2021-02-11 17:59:29 -0500

    added router post

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -14,11 +14,19 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   //grab data from req.body to get what is in front end.
+  let newNote = req.body;
   fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) =>{
     if (err) throw err;
-    console.log(data)
+    //console.log(data)
     // parse data and push req.body (new array)
+    data =JSON.parse(data);
+    data.push(newNote);
+    console.log(data);
     // call fs.writeFile with new array
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), (err) => {
+      if (err) throw err;
+      res.send(data);
+    })
   } )
 });
 
